ba97a4ba22b2c14d3ced0850f28db289e5f5f7aa
Fedomn <fedomn.ma@gmail.com>
2022-01-28 20:42:26 +0800

    add tokio channel

--- playground/src/tokio.rs
+++ playground/src/tokio.rs
@@ -1,6 +1,31 @@
+/// https://tokio.rs/tokio/tutorial/channels
 #[cfg(test)]
 mod tokio_test {
 
+    // multi-producer, single-consumer channel. Many values can be sent.
+    #[tokio::test]
+    async fn mpsc_channel_test() {
+        use tokio::sync::mpsc;
+        use tokio::time::{sleep, Duration};
+
+        let (tx, mut rx) = mpsc::channel(1);
+        let tx2 = tx.clone();
+
+        tokio::spawn(async move {
+            sleep(Duration::from_millis(200)).await;
+            tx.send(1).await.unwrap();
+        });
+
+        tokio::spawn(async move {
+            sleep(Duration::from_millis(500)).await;
+            tx2.send(2).await.unwrap();
+        });
+
+        assert_eq!(rx.recv().await.unwrap(), 1);
+        assert_eq!(rx.recv().await.unwrap(), 2);
+    }
+
+    // single-producer, single consumer channel. A single value can be sent.
     #[tokio::test]
     async fn oneshot_channel_test() {
         use tokio::sync::oneshot;
@@ -23,5 +48,27 @@ mod tokio_test {
                 }
             }
         }
+    }
+
+    // multi-producer, multi-consumer. Many values can be sent. Each receiver sees every value.
+    #[tokio::test]
+    async fn broadcast_channel_test() {
+        use tokio::sync::broadcast;
+
+        let (tx, mut rx1) = broadcast::channel(16);
+        let mut rx2 = tx.subscribe();
+
+        tokio::spawn(async move {
+            assert_eq!(rx1.recv().await.unwrap(), 10);
+            assert_eq!(rx1.recv().await.unwrap(), 20);
+        });
+
+        tokio::spawn(async move {
+            assert_eq!(rx2.recv().await.unwrap(), 10);
+            assert_eq!(rx2.recv().await.unwrap(), 20);
+        });
+
+        tx.send(10).unwrap();
+        tx.send(20).unwrap();
     }
 }
