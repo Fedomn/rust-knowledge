1cfebf4cb873d5743226e8164e87c9a3b6201d66
Fedomn <fedomn.ma@gmail.com>
2022-01-28 18:55:26 +0800

    add tokio oneshot channel

--- playground/src/concurrency.rs
+++ playground/src/concurrency.rs
@@ -1,8 +1,8 @@
 #[cfg(test)]
 mod concurrency {
-    use std::{sync, thread};
     use std::borrow::Borrow;
     use std::sync::Arc;
+    use std::{sync, thread};
     use sync::Mutex;
 
     #[test]
@@ -37,6 +37,30 @@ mod concurrency {
 
         for recv in rx {
             println!("Got: {}", recv)
+        }
+    }
+
+    #[tokio::test]
+    async fn oneshot_channel_test() {
+        use tokio::sync::oneshot;
+        use tokio::time::{interval, sleep, Duration};
+
+        let (send, mut recv) = oneshot::channel();
+        let mut interval = interval(Duration::from_millis(100));
+
+        tokio::spawn(async move {
+            sleep(Duration::from_secs(1)).await;
+            send.send("shut down").unwrap();
+        });
+
+        loop {
+            tokio::select! {
+                _ = interval.tick() => println!("Another 100ms"),
+                msg = &mut recv => {
+                    println!("Got message: {}", msg.unwrap());
+                    break;
+                }
+            }
         }
     }
 
