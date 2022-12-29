2e603cb7108f1fd11773957167bccb9c9b32fee4
Fedomn <fedomn.ma@gmail.com>
2022-12-29 12:38:19 +0800

    add generic as type example

--- playground/src/basic/generic.rs
+++ playground/src/basic/generic.rs
@@ -29,4 +29,48 @@ mod generic_test {
 
         assert_eq!(user.id.inner, product.id.inner);
     }
+
+    #[test]
+    fn generic_as_types_test() {
+        // https://rust-unofficial.github.io/patterns/functional/generics-type-classes.html
+
+        #[derive(Debug, Default)]
+        pub struct Identifier<T> {
+            inner: T,
+        }
+
+        impl<T> Identifier<T> {
+            pub fn new(inner: T) -> Self {
+                Self { inner }
+            }
+        }
+
+        impl<T> Identifier<T>
+        where
+            T: Copy,
+        {
+            // only for type that impl copy trait
+            pub fn id(&self) -> T {
+                self.inner
+            }
+        }
+
+        pub type UserId<'a> = Identifier<&'a str>;
+        pub type ProductId = Identifier<String>;
+        impl<T> Identifier<T>
+        where
+            T: Clone,
+        {
+            pub fn clone_id(&self) -> T {
+                self.inner.clone()
+            }
+        }
+
+        let num = Identifier::<i32>::new(12);
+        assert!(num.id() == 12);
+        let userid = UserId::new("userid");
+        assert!(userid.id() == "userid");
+        let productid = ProductId::new("productid".to_string());
+        assert!(productid.clone_id() == "productid");
+    }
 }
